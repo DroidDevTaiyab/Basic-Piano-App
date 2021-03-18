f4f83e27aeba180cb341106edcce29ddb63a7898
ali <taiyaba@siliconasiaworks.com>
2021-03-19 01:21:05 +0530

    init commit

---
+++ app/src/main/java/com/taiyabali/basicpianoapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.taiyabali.basicpianoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.taiyabali.basicpianoapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding;
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.button.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this, "A Click", Toast.LENGTH_SHORT).show()
+        })
+    }
+}
